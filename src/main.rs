7ac3698ed0604700f639afd998ad4e377ac35b49
Sourabh Marathe <surja795@gmail.com>
2020-05-02 07:52:40 -0400

    Add single threaded server

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,19 @@ use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use hello::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
-            let stream = stream.unwrap();
+        let stream = stream.unwrap();
 
+        pool.execute( || {
             handle_connection(stream);
-        }
+        });
+    }
 }
 
  fn handle_connection(mut stream: TcpStream) {
